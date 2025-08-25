9fa5a34b25181c902f18b9eb20d99bd4327e5424
choiguen <daindj0@gmail.com>
2025-08-25 14:24:19 +0900

    loading 추가

--- status.py
+++ status.py
@@ -1,12 +1,19 @@
 import random
+import time
 
-
-while True:
+while True :
     str = random.randint(1,100)
     int = random.randint(1,100)
     dex = random.randint(1,100)
-    if str+int+dex != 100:
-        continue
-    else:
+    if (str+int+dex) != 100:
         print(f'힘:{str}, 지능{int}, 민첩{dex}')
         break
+
+# loading : 다운로드 되는 느낌
+
+for i in range(30):
+    time.sleep(random.random())
+    print('=', end='', flush=True) #GPT 제안 : print('\r[' + '='*(i+1) + ' '*(30-i-1) + ']', end='', flush=True)
+print('로딩 종료')
+
+#GTP 제안 해석 : 문자 '='가 지정해놓은 공백에 루프마다 쌓이면서 출력.
